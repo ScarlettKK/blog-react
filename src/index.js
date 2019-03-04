434baec5ceea4b194f0e38c7347f6d25edae9f94
Scarlett <scarlettkkszekai@gmail.com>
2019-03-04 17:19:47 +0800

    Initialize project, learn about styled-components, injectGlobal and reset-css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css'; // 要注意这里的css引入会跟其他文件（组件）里面的样式互相冲突，所以这里不建议直接引入css样式
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './style.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
