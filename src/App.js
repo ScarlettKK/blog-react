c4ccb2f41f41714a5c2e2f89faec8e6c292127fc
Scarlett <scarlettkkszekai@gmail.com>
2019-03-07 21:47:02 +0800

    use styled-components to finish the style of blog header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css'; 
+import Header from './common/header/index'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Blog-React
-      </div>
-    );
+      <Header />
+    )
   }
 }
 
